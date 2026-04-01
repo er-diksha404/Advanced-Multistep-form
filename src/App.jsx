ab459692421a3d46aa1ba477b822874fb829ee1a
Diksha Soni <soni.er.diksha509@gmail.com>
2026-04-01 14:56:27 +0530

    Updated app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import MultiStepForm from "./pages/MultiStepForm";
 
 //library use for Toast notification
-import { Toaster } from "react-hot-toast";
+// import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 to-gray-100 flex items-center justify-center px-4">
       
       <div className="w-full max-w-lg bg-white p-8 rounded-2xl shadow-xl">
-        <Toaster position="top-right" reverseOrder={false} />
+        {/* <Toaster position="top-right" reverseOrder={false} /> */}
         <MultiStepForm />
       </div>
 
